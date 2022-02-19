5894095c552d3b765726ac8c82a12af77d0ff2c1
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-02-19 15:18:00 -0700

    first formation of code

---
+++ game/casting/fallen_item.py
@@ -0,0 +1,14 @@
+from game.casting.actor import Actor
+
+# TODO: Implemented the falling item here
+
+class Artifact(Actor):
+    def __init__(self):
+        super().__init__()
+        self._message = ""    
+        
+    def set_message(self, message):
+        self._message = message
+        
+    def get_message(self):
+        return self._message
