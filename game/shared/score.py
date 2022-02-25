1dbb5c0de72d2275421cb6a7c3d09a9394b8958d
Emma <87395798+emw123@users.noreply.github.com>
2022-02-25 10:32:07 -0700

    Score

--- game/shared/score.py
+++ game/shared/score.py
@@ -1,5 +1,55 @@
-
+import pyray
 
 class Score:
-    def __init__():
-        pass
+    def __init__(self, caption, width, height, cell_size, frame_rate, debug = False):
+        self._caption = caption
+        self._width = width
+        self._height = height
+        self._cell_size = cell_size
+        self._frame_rate = frame_rate
+        self._debug = debug
+
+    def close_window(self):
+        """Closes the window and releases all computing resources."""
+        pyray.close_window()
+
+    def get_cell_size(self):
+        """Gets the video screen's cell size.
+        
+        Returns:
+            Grid: The video screen's cell size.
+        """
+        return self._cell_size
+
+    def get_height(self):
+        """Gets the video screen's height.
+        
+        Returns:
+            Grid: The video screen's height.
+        """
+        return self._height
+
+    def get_width(self):
+        """Gets the video screen's width.
+        
+        Returns:
+            Grid: The video screen's width.
+        """
+        return self._width
+
+    def is_window_open(self):
+        """Whether or not the window was closed by the user.
+
+        Returns:
+            bool: True if the window is closing; false if otherwise.
+        """
+        return not pyray.window_should_close()
+
+    def open_window(self):
+        """Opens a new window with the provided title.
+
+        Args:
+            title (string): The title of the window.
+        """
+        pyray.init_window(self._width, self._height, self._caption)
+        pyray.set_target_fps(self._frame_rate)
